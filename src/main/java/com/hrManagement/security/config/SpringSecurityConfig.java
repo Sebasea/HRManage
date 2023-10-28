28bb0468fd9d5ed2a18653c431e57e61ecd18e18
Juanes191 <juanesvilla2016@gmail.com>
2023-10-28 10:33:10 -0500

    Add basic auth

---
+++ src/main/java/com/hrManagement/security/config/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.hrManagement.security.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SpringSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("Sabana")
+                .password("JJJ2023")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/message")
+                        .authenticated()
+                )
+                .httpBasic();
+        return http.build();
+    }
+}
